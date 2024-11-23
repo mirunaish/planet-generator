0e9933a4a3fe17acea359bd70cf152b9d2d0663d
sangha-jang <sangha.jang.25@dartmouth.edu>
2024-11-23 14:57:33 -0500

    added more trees

--- src/chunk.js
+++ src/chunk.js
@@ -12,6 +12,8 @@ class Chunk {
     this.generateTrees(); 
     this.generateSmallTrees();
     this.generateLargeTrees();
+    this.generateBushyTrees();
+    this.generateCurvyTrees();
   }
 
   //create bamboo trees at different locations
@@ -79,6 +81,52 @@ class Chunk {
         0.1, // Radius
         8, // Resolution
         0.0 // Decay Factor
+      );
+      this.trees.push(tree);
+    }
+  }
+
+  //generate bushy trees at different locations
+  generateBushyTrees() {
+    const treePositions = [
+      { x: this.center.x + 15, y: -2, z: this.center.z + 15 },
+      { x: this.center.x - 15, y: -2, z: this.center.z - 15 },
+    ];
+
+    for (const pos of treePositions) {
+      const tree = new LSystemTree(
+        pos,
+        "F", // Axiom
+        { F: "F[+F[+F]]F[-F[-F]]" }, // Rule
+        3, // Iterations
+        2, // Length
+        20, // Angle
+        0.1, // Radius
+        8, // Resolution
+        0.5 // Decay Factor
+      );
+      this.trees.push(tree);
+    }
+  }
+
+  //generate curvy trees at different locations
+  generateCurvyTrees() {
+    const treePositions = [
+      { x: this.center.x + 20, y: -1, z: this.center.z + 5 },
+      { x: this.center.x - 20, y: -1, z: this.center.z - 5 },
+    ];
+
+    for (const pos of treePositions) {
+      const tree = new LSystemTree(
+        pos,
+        "F", // Axiom
+        { F: "F → FF-[-F+F+F]+[+F-F-F]" }, // Rule
+        2, // Iterations
+        0.5, // Length
+        15, // Angle
+        0.1, // Radius
+        8, // Resolution
+        0.5 // Decay Factor
       );
       this.trees.push(tree);
     }
